bbaeae6c01788e48f21ff19b98e0ce50fdf65ead
Carmen Alicia Olascoaga <colascoaga@unal.edu.co>
2025-01-06 20:14:03 -0500

    Final

--- homework/pregunta_04.py
+++ homework/pregunta_04.py
@@ -27,7 +27,7 @@ def pregunta_04():
 
     """
     registro_mes = {}
-    with open('files\\input\\data.csv', 'r') as file:
+    with open('files/input/data.csv', 'r') as file:
         for line in file:
             columnas = line.strip(). split ('\t')
             if len(columnas) > 2:
--- homework/pregunta_05.py
+++ homework/pregunta_05.py
@@ -16,7 +16,7 @@ def pregunta_05():
 
     """
     letras = {}
-    with open('files\\input\\data.csv', 'r') as file:
+    with open('files/input/data.csv', 'r') as file:
         for line in file:
             columnas = line.strip(). split ('\t')
             if len(columnas) > 1:
